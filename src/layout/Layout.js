6364b60193aa4038affa79b1031a788b99f91e17
DESKTOP-OELESH2\hi <sml2031@gmail.com>
2023-09-22 17:38:13 +0900

    0922

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,10 +1,20 @@
-import Header from '../component/Header';
+
 import '../css/common.css';
+import Header from '../component/Header';
+import Socialkakao from '../component/kakaologin'
+import Unlogin from '../component/unlogin'
+import Footer from '../component/footer'
+import Home from '../component/home'
 
 function Layout() {
   return (
     <>
+      
       <Header />
+      <Home />
+      <Socialkakao />
+      <Unlogin />
+      <Footer />
     </>
   );
 }
