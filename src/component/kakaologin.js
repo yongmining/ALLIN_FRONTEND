af28553bd965bab5bc2d609a539a0e3ff13f0da5
UICHEOL_HAWNG <gpsyo911@gmail.com>
2023-09-25 20:56:53 +0900

    feat]feedback, modal, clinic 등

--- src/component/kakaologin.js
+++ src/component/kakaologin.js
@@ -1,18 +1,18 @@
-const Socialkakao = ()=>
-{
-    const Rest_api_key='96fc49ad6635ee98b7975b4e00cd5322' //REST API KEY
-    const redirect_uri = 'http://localhost:3000/auth' //Redirect URI
-    // oauth 요청 URL
-    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
-    const handleLogin = ()=>{
-        window.location.href = kakaoURL
-    }
-    return(
+const Socialkakao = () => {
+  const Rest_api_key = '96fc49ad6635ee98b7975b4e00cd5322'; //REST API KEY
+  const redirect_uri = 'http://localhost:3000/auth'; //Redirect URI
+  // oauth 요청 URL
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
+  const handleLogin = () => {
+    window.location.href = kakaoURL;
+  };
+  return (
     <>
-        <div className="kakaologin">
-            <button onClick={handleLogin}>카카오 로그인</button>
-        </div>
+      <div className="kakaologin">
+        <button onClick={handleLogin}>카카오 로그인</button>
+        .........{' '}
+      </div>
     </>
-    )
-}
-export default Socialkakao
+  );
+};
+export default Socialkakao;
