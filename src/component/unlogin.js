fa4d1d77193923a4d52776ee94e0619b49db14bc
UICHEOL_HAWNG <gpsyo911@gmail.com>
2023-09-26 14:17:32 +0900

    no message

--- src/component/unlogin.js
+++ src/component/unlogin.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import Modal from "./unloginModal";
+import React, { useState } from 'react';
+import Modal from './modal/unloginModal';
 
 function Unlogin() {
   const [isModalOpen, setIsModalOpen] = useState(false);
