abe5d9e52ac7d19e21293a6447e13379f720d553
UICHEOL_HAWNG <gpsyo911@gmail.com>
2023-09-26 15:20:30 +0900

    gg

--- src/page/home.js
+++ src/page/home.js
@@ -1,5 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+
+const KAKAO_REST_API_KEY = '96fc49ad6635ee98b7975b4e00cd5322';
+const KAKAO_REDIRECT_URI = `http://localhost:3000/auth`;
+const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+const loginHandler = (platform) => {
+  switch (platform) {
+    case 'N':
+      // window.location.href =
+      break;
+    case 'K':
+      window.location.href = KAKAO_AUTH_URI;
+      break;
+    default:
+      break;
+  }
+};
 
 const Home = () => {
   return (
@@ -9,7 +24,13 @@ const Home = () => {
       </div>
       <div className="home">
         <p>감정을 보여주세요</p>
-        <Link to="./profile">프로필</Link>
+        <div>
+          <div onClick={() => loginHandler('K')}>
+            <img src="../../../img/kakaoLogin.png" alt="카카오 로그인" />
+          </div>
+          <br />
+          <button onClick={() => loginHandler('N')}>비회원 로그인</button>
+        </div>
       </div>
     </div>
   );
