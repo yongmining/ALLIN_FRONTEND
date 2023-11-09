35e213ba215123536a9d4030163aef7f52f323bb
DESKTOP-K7FN4SI\hi <choemyeonggeon@gmail.com>
2023-11-09 14:41:32 +0900

    feature/mg

--- src/api/emotionApi.js
+++ src/api/emotionApi.js
@@ -18,3 +18,31 @@ export const memberEmotion = (memberNo) => {
     }
   };
 };
+
+export const memberEmotionCount = (memberNo) => {
+  let URL = `http://localhost:8080/api/v1/emotion/member/${memberNo}`;
+
+  return async (dispatch) => {
+    try {
+      const response = await fetch(URL, {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json",
+          Accept: "*/*",
+        },
+      });
+      const result = await response.json();
+
+      if (response.status === 200) {
+        dispatch({ type: GET_MEMBER_EMOTION, payload: result });
+        return result; // 성공 결과를 반환
+      } else {
+        // response가 성공적이지 않은 경우 오류 처리
+        throw new Error(result.message || "Error fetching emotions");
+      }
+    } catch (error) {
+      // 에러를 적절히 처리하거나, 호출부에 에러를 전파할 수 있습니다.
+      return Promise.reject(error);
+    }
+  };
+};
